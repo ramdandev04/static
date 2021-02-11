793c41f7354b6862bf3bba491da71f9ab517c705
Ramdani <77159138+ramdandev04@users.noreply.github.com>
2021-02-11 21:25:01 +0700

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,15 @@
+apt update
+apt upgrade -y
+sleep
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash && source ~/.bashrc
+sleep 2
+clear
+sleep 2
+echo 'try'
+nvm install v14.15.5
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64,s390x ] http://repo.mongodb.com/apt/ubuntu focal/mongodb-enterprise/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
+apt update
+sudo apt-get install -y mongodb-enterprise
+sudo systemctl start mongod
+sudo systemctl enable mongod
