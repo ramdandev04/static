dbefc3f2d9e1d090e80ec803927878342c9d7c90
Ramdani <77159138+ramdandev04@users.noreply.github.com>
2021-02-11 14:02:31 +0700

    Update 145captha.js

--- js/145captha.js
+++ js/145captha.js
@@ -1,5 +1,6 @@
 var widget;
 var onloadCallback = () => {
+    alert('loaded')
     widget = grecaptcha.render('captha', {
         sitekey: '6LekPFMaAAAAABw06APLgb7AaAvbz3-ix4TTQ3XL',
         theme: 'dark'
