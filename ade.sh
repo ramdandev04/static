01bd1468b522257a10092f08af05a2a9ec93e3bf
Ramdani <77159138+ramdandev04@users.noreply.github.com>
2021-02-11 20:50:34 +0700

    Update ade.sh

--- ade.sh
+++ ade.sh
@@ -1,2 +1,9 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install v14.15.5
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64,s390x ] http://repo.mongodb.com/apt/ubuntu focal/mongodb-enterprise/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
 apt update
-apt upgrade -y
+sudo apt-get install -y mongodb-enterprise
+sudo systemctl start mongod
+sudo systemctl enable mongod
