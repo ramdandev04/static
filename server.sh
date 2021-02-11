58485890714a562c336af525ae801e7a4c883500
Ramdani <77159138+ramdandev04@users.noreply.github.com>
2021-02-11 21:41:03 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,6 +3,9 @@ apt update
 apt upgrade -y
 sleep
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash && source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 sleep 2
 clear
 sleep 2
